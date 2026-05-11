268a94828ee206a5601223a182db44591243e4f6
Sagar Srivastava <sagarsrv247@gmail.com>
2026-05-11 22:13:06 +0530

    third commit

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -21,4 +21,4 @@ test("Verify Google title", async ({page}) => {
 
 })
 
-//this is my first testcase.
+//this is my first Playwright test. I'm new to playwright. I want to learn how to write tests using Playwright. I will start with a simple test that verifies the title of the Google homepage. I will use the 'test' function from Playwright to define my test, and I will use the 'expect' function to make assertions about the title. I will also use the 'page' fixture to interact with the browser..
