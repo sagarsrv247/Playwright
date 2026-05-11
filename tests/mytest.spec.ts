687e586c8de04031ea3689873c9cdc20f61ce25f
Sagar Srivastava <sagarsrv247@gmail.com>
2026-05-11 21:59:28 +0530

    Initial commit

---
+++ tests/mytest.spec.ts
@@ -0,0 +1,19 @@
+import {test,expect} from '@playwright/test';
+
+/*
+test("title", () => {
+//step 1
+//step 2
+// step 3
+})
+*/
+
+//fixture - global variable : page, browser
+
+
+test("Verify Google title", ({page}) => {
+
+
+
+
+})
